dcfa683d6a8a6c93bd4204059b94732f63786042
AvinavTyagi <avinav.tyagi@sjsu.edu>
2019-11-11 20:46:45 -0800

    added highlighting of attributes

--- runtimeterror/src/components/Dashboard.js
+++ runtimeterror/src/components/Dashboard.js
@@ -8,10 +8,10 @@ let dataBar1 = {
   datasets: [
     {
       label: "Nasa Budget (in millions)",
-      backgroundColor: "rgba(255,99,132,0.2)",
+      backgroundColor: ["rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,200,11,0.2)"],
       borderColor: "rgba(255,99,132,1)",
       borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBackgroundColor: ["rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,200,11,0.5)"],
       hoverBorderColor: "rgba(255,99,132,1)",
       data: []
     }
@@ -23,10 +23,10 @@ let dataBar2 = {
   datasets: [
     {
       label: "Nasa Budget (in millions)",
-      backgroundColor: "rgba(255,99,132,0.2)",
+      backgroundColor: ["rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,200,11,0.2)"],
       borderColor: "rgba(255,99,132,1)",
       borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBackgroundColor: ["rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,200,11,0.5)"],
       hoverBorderColor: "rgba(255,99,132,1)",
       data: []
     }
@@ -38,10 +38,10 @@ let dataBar3 = {
   datasets: [
     {
       label: "Project Budget (in billions)",
-      backgroundColor: "rgba(255,99,132,0.2)",
+      backgroundColor: ["rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,99,132,0.2)","rgba(255,200,11,0.2)"],
       borderColor: "rgba(255,99,132,1)",
       borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBackgroundColor: ["rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,99,132,0.4)","rgba(255,200,11,0.5  )"],
       hoverBorderColor: "rgba(255,99,132,1)",
       data: []
     }
@@ -55,19 +55,19 @@ let linedata1 = {
       label: "Nasa satellites launches",
       fill: false,
       lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
+      backgroundColor: ["rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,100,11,1)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)"],
       borderColor: "rgba(75,192,192,1)",
       borderCapStyle: "butt",
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
+      pointBorderColor: ["rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,100,11,1)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)"],
       pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
+      pointBorderWidth: [1,1,1,5,1,1,1],
       pointHoverRadius: 5,
       pointHoverBackgroundColor: "rgba(75,192,192,1)",
       pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
+      pointHoverBorderWidth: [2,2,2,5,2,2,2],
       pointRadius: 1,
       pointHitRadius: 10,
       data: []
@@ -115,13 +115,13 @@ let planetData = {
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
+      pointBorderColor: ["rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,192,192,0.4)","rgba(75,100,11,1)"],
       pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
+      pointBorderWidth: [1,1,1,1,1,1,1,5],
       pointHoverRadius: 5,
       pointHoverBackgroundColor: "rgba(75,192,192,1)",
       pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
+      pointHoverBorderWidth: [2,2,2,2,2,2,2,5],
       pointRadius: 1,
       pointHitRadius: 10,
       data: []
